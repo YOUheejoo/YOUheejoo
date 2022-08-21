0d76a776b1aed7f0c57b38212863123d0a616ad0
YOUheejoo <103569427+YOUheejoo@users.noreply.github.com>
2022-08-21 23:08:46 +0900

    Add files via upload

--- nameless.py
+++ nameless.py
@@ -1,60 +1,148 @@
+import os
 import pygame
 
+#버블 클래스 생성
+class Bubble(pygame.sprite.Sprite):
+    def __init__(self, image, color, position):
+        super().__init__()
+        self.image = image
+        self.color = color
+        self.rect = image.get_rect(center=position)
+
+#발사대 클래스 생성
+class Pointer(pygame.sprite.Sprite):
+    def __init__(self, image, position, angle):
+        super().__init__()
+        self.image = image
+        self.rect = image.get_rect(center=position)
+        self.angle = angle
+        self.original_image = image
+        self.position = position
+
+    def draw(self, screen):
+        screen.blit(self.image, self.rect)
+        pygame.draw.circle(screen, RED, self.position, 3)
+
+    #회전
+    def rotate(self, angle):
+        self.angle += angle
+
+        if self.angle > 170:
+            self.angle = 170
+        elif self.angle < 10:
+            self.angle = 10
+
+        self.image = pygame.transform.rotozoom(self.original_image, self.angle, 1)
+        self.rect = self.image.get_rect(center=self.position)
+
+#맵 만들기
+def setup():
+    global map 
+    map = [
+        list("RRYYBBGG"),
+        list("RRYYBBG/"),
+        list("BBGGRRYY"),
+        list("BGGRRYY/"),
+        list("........"),
+        list("......./"),
+        list("........"),
+        list("......./"),
+        list("........"),
+        list("......./"),
+        list("........")
+    ]
+
+    for row_idx, row in enumerate(map): 
+        for col_idx, col in enumerate(row):
+            if col in [".", "/"]:
+                continue
+            position = get_bubble_position(row_idx, col_idx)
+            image = get_bubble_image(col)
+            bubble_group.add(Bubble(image, col, position))
+
+def get_bubble_position(row_idx, col_idx):
+    pos_x = col_idx * CELL_SIZE + (BUBBLE_WIDTH // 2)
+    pos_y = row_idx * CELL_SIZE + (BUBBLE_HEIGHT // 2)
+    if row_idx % 2 == 1:
+        pos_x += CELL_SIZE // 2
+    return pos_x, pos_y
+
+def get_bubble_image(color):
+    if color == "R" :
+        return bubble_images[0]
+    elif color == "Y" :
+        return bubble_images[1]
+    elif color == "B" :
+        return bubble_images[2]
+    elif color == "G" :
+        return bubble_images[3]
+    elif color == "P" :
+        return bubble_images[4]
+    else:
+        return bubble_images[-1]
+
 pygame.init() 
-
-screen_width = 500
-screen_height = 700
+screen_width = 448
+screen_height = 680
 screen = pygame.display.set_mode((screen_width, screen_height))
-background = pygame.image.load("C:\\Users\\heejo\\Desktop\\희주코딩\\background.png")
 pygame.display.set_caption('justgame')
+clock = pygame.time.Clock()
 
-character = pygame.image.load("C:\\Users\\heejo\\Desktop\\코딩\\929fd0c5ed77d3b.png")
-character_size = character.get_rect().size
-character_width = character_size[0]
-character_height = character_size[1]
-character_x_pos = (screen_width / 2) - (character_width/2)
-character_y_pos = screen_height - character_height
-to_x = 0
-to_y = 0
-character_speed = 1
+current_path = os.path.dirname(__file__)
+background = pygame.image.load(os.path.join(current_path, "background.png"))
+
+bubble_images = [
+    pygame.image.load(os.path.join(current_path, "red.png")).convert_alpha(),
+    pygame.image.load(os.path.join(current_path, "yellow.png")).convert_alpha(),
+    pygame.image.load(os.path.join(current_path, "blue.png")).convert_alpha(),
+    pygame.image.load(os.path.join(current_path, "green.png")).convert_alpha(),
+    pygame.image.load(os.path.join(current_path, "purple.png")).convert_alpha(),
+    pygame.image.load(os.path.join(current_path, "black.png")).convert_alpha()
+]
+
+#발사대 이미지 불러오기
+pointer_image = pygame.image.load(os.path.join(current_path, "pointer.png"))
+pointer = Pointer(pointer_image, (screen_width // 2, 624), 90)
+
+#게임 관련 변수
+CELL_SIZE = 56
+BUBBLE_WIDTH = 56
+BUBBLE_HEIGHT = 62
+RED = (255, 0, 0)
+
+#화살표 관련 변수
+to_angle_left = 0
+to_angle_right = 0
+angle_speed = 1.5 #1.5도씩 움직임
+
+map = [] #맵
+bubble_group = pygame.sprite.Group()
+setup()
 
 running = True 
 while running:
+    clock.tick(60) #FPS 60
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                to_x -= 1
+                to_angle_left += angle_speed
             elif event.key == pygame.K_RIGHT:
-                to_x += 1
-            elif event.key == pygame.K_DOWN:
-                to_y += 1
-            elif event.key == pygame.K_UP:
-                to_y -= 1
-
+                to_angle_right -= angle_speed
+        
         if event.type == pygame.KEYUP:
-            if event.key == pygame.K.LEFT or event.key == pygame.K.RIGHT:
-                to_x = 0
-            if event.key == pygame.K.DOWN or event.key == pygame.K.UP:
-                to_y = 0
-    character_x_pos += to_x *60
-    character_y_pos -= to_x *60
+            if event.key == pygame.K_LEFT:
+                to_angle_left = 0
+            elif event.key == pygame.K_RIGHT:
+                to_angle_right = 0
 
     screen.blit(background, (0,0))
+    bubble_group.draw(screen)
+    pointer.rotate(to_angle_right + to_angle_left)
+    pointer.draw(screen)
     pygame.display.update()
-
-    if character_x_pos < 0:
-        character_x_pos = 0
-    
-    elif character_x_pos > screen_width - character_width:
-        character_x_pos = screen_width - character_width
-
-    if character_y_pos < 0:
-        character_y_pos = 0
-    
-    elif character_y_pos > screen_height - character_height:
-        character_y_pos = screen_height - character_height
 
 pygame.quit()
